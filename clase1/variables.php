27866c21184f1ffd34cbd8b635d28257d25c93bc
Marcos <exegeses@gmail.com>
2021-10-02 13:11:39 -0300

    varianles, copntsnates y concatenación

--- clase1/variables.php
+++ clase1/variables.php
@@ -2,7 +2,20 @@
 
     $numero = 137;
     $texto = 'cadena de caractéres';
+    const NOMBRE = 'Rick';
+    //define('NOMBRE', 'Rick');
+    const APELLIDO = 'Sanchez';
+    //define('APELLIDO', 'Sanchez');
+    $precio = 5000;
+    $sql = "SELECT prdNombre, prdPrecio 
+                WHERE prdPrecio < ".$precio;
 ?>
 <br>
 <?php
-    echo $numero;
+    //echo NOMBRE.' '.APELLIDO;
+    echo NOMBRE, ' ', APELLIDO;
+?>
+<hr>
+<?php
+    //unset($numero);  //unset() sirve para eliminar variables
+    echo 'el númnero es: ', $numero;

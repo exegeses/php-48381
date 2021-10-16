e3dcffc1d56d61a681e05a8f1219731b4b0e8430
Marcos <exegeses@gmail.com>
2021-10-16 11:20:10 -0300

    Práctica locaciones v3 sin terminar

---
+++ clase3/locaciones3.php
@@ -0,0 +1,52 @@
+<?php
+    $locaciones2 =
+        [
+            'Cambodia'=>'angkor',
+            'Turquía'=>'azul',
+            'Rusia'=>'basil',
+            'Dubai'=>'burj',
+            'Italia'=>'colosseo',
+            'Chile'=>'easter',
+            'Francia'=>'eiffel',
+            'Egipto'=>'gizah',
+            'Vietnam'=>'ha-long',
+            'USA'=>'liberty',
+            'Peru'=>'machu',
+            'Australia'=>'opera',
+            'Tailandia'=>'palace',
+            'Jordania'=>'petra',
+            'España'=>'sagrada',
+            'Grecia'=>'santorini',
+            'India'=>'taj',
+            'China'=>'wall'
+        ];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Locaciones</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    <main>
+
+<?php
+
+?>
+        <article>
+            <img src="locaciones/acropolis.jpg">
+            <h2>Nombre</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi at atque beatae, corporis deserunt dolorem eius eveniet ex harum, magni obcaecati qui ratione veritatis.
+            </p>
+        </article>
+<?php
+
+?>
+
+    </main>
+</body>
+</html>

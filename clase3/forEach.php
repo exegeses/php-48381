122ac882c91cff12c9cdafcae9528338599baaad
Marcos <exegeses@gmail.com>
2021-10-16 11:27:32 -0300

    Ejemplo bucle foreEach

---
+++ clase3/forEach.php
@@ -0,0 +1,10 @@
+<?php
+    $marcas = [
+                'Hermes', 'Armani', 'Boss',
+                'Zara', 'UniQlo', 'Puma'
+                ];
+
+    //foreach ( $coleccion as $auxiliar )
+    foreach ( $marcas as $marca ){
+        echo $marca, '<br>';
+    }

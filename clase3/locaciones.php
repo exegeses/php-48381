4b8b6b7700bbbc336241d9c53dfdef333fa828c2
Marcos <exegeses@gmail.com>
2021-10-16 10:40:56 -0300

    Práctica locaciones v1 sin terminar

--- clase3/locaciones.php
+++ clase3/locaciones.php
@@ -1,3 +1,14 @@
+<?php
+    $locaciones =
+        [
+            'angkor', 'azul', 'basil', 'burj',
+            'colosseo', 'easter', 'eiffel',
+            'gizah', 'ha-long', 'liberty',
+            'machu', 'opera', 'palace', 'petra',
+            'sagrada', 'santorini', 'taj',
+            'wall'
+        ];
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,8 +16,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Locaciones</title>
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
+    <main>
 
+        <article>
+            <img src="locaciones/acropolis.jpg">
+            <h2>Nombre</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi at atque beatae, corporis deserunt dolorem eius eveniet ex harum, magni obcaecati qui ratione veritatis.
+            </p>
+        </article>
+
+
+    </main>
 </body>
 </html>

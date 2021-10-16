f8ee13d44bd25831fa8dc04d38e7e2b886461dd7
Marcos <exegeses@gmail.com>
2021-10-16 10:58:18 -0300

    Práctica locaciones v1 terminada

--- clase3/locaciones.php
+++ clase3/locaciones.php
@@ -8,6 +8,7 @@
             'sagrada', 'santorini', 'taj',
             'wall'
         ];
+    $cantidad = count($locaciones);
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,14 +22,19 @@
 <body>
     <main>
 
+<?php
+    for( $n = 0; $n < $cantidad; $n++  ){
+?>
         <article>
-            <img src="locaciones/acropolis.jpg">
+            <img src="locaciones/<?= $locaciones[$n] ?>.jpg">
             <h2>Nombre</h2>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi at atque beatae, corporis deserunt dolorem eius eveniet ex harum, magni obcaecati qui ratione veritatis.
             </p>
         </article>
-
+<?php
+    }
+?>
 
     </main>
 </body>

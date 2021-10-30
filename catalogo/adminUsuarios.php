8de37ba3a4d5b65d78a1336d708948ac6a2a181b
Marcos <exegeses@gmail.com>
2021-10-30 13:57:38 -0300

    Panel de administración de usuarios

---
+++ catalogo/adminUsuarios.php
@@ -0,0 +1,59 @@
+<?php
+    //require 'config/config.php';
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Panel de administración de usuarios</h1>
+
+        <a href="admin.php" class="btn btn-outline-secondary btn-sm my-2">
+            <i class="bi bi-chevron-left"></i>
+            Volver a dashboard
+        </a>
+
+        <table class="table table-borderless table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                    <th colspan="2">
+                        <a href="" class="btn btn-outline-secondary btn-sm">
+                            <i class="bi bi-plus-square"> </i>
+                            Agregar
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>nombre</td>
+                    <td>apellido</td>
+                    <td>email@mail.com</td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary btn-sm">
+                            <i class="bi bi-pencil-square"> </i>
+                            Modificar
+                        </a>
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary btn-sm">
+                            <i class="bi bi-dash-square"> </i>
+                            Eliminar
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <a href="admin.php" class="btn btn-outline-secondary btn-sm my-2">
+            <i class="bi bi-chevron-left"></i>
+            Volver a dashboard
+        </a>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>

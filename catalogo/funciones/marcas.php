bd8a51042341f5e28560f4386f621b41a759b800
Marcos <exegeses@gmail.com>
2021-10-30 12:16:43 -0300

    función agregarMarca()

--- catalogo/funciones/marcas.php
+++ catalogo/funciones/marcas.php
@@ -15,7 +15,17 @@
     function verMarcaPorID()
     {}
     function agregarMarca()
-    {}
+    {
+        $mkNombre = $_POST['mkNombre'];
+        $link = conectar();
+        $sql  = "INSERT INTO marcas 
+                        ( mkNombre ) 
+                    VALUES 
+                       ( '".$mkNombre."' )";
+        $resultado = mysqli_query( $link, $sql )
+                        or die( mysqli_error( $link ) );
+        return $resultado;
+    }
     function modificarMarca()
     {}
     function eliminarMarca()
